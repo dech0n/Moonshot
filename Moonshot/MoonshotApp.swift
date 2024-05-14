dd1a6a497562a757093b076a672599420fc49f78
Dechon <72828058+dech0n@users.noreply.github.com>
2024-05-14 10:26:01 -0700

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Dechon Ryan on 5/14/24.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

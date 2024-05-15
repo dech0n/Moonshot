0f644d57ba77ba44d10b135efec3b091e33221c1
Dechon <72828058+dech0n@users.noreply.github.com>
2024-05-15 15:28:42 -0700

    Apply astronaut view

--- Moonshot/MissionView.swift
+++ Moonshot/MissionView.swift
@@ -47,7 +47,7 @@ struct MissionView: View {
                     HStack {
                         ForEach(crew, id: \.role) { crewMember in
                             NavigationLink {
-                                Text("Astronaut details")
+                                AstronautView(astronaut: crewMember.astronaut)
                             } label: {
                                 HStack {
                                     Image(crewMember.astronaut.id)

fc9608918933d723ace16a00c74046ebeb8a7532
Dechon <72828058+dech0n@users.noreply.github.com>
2024-05-15 15:20:45 -0700

    Create custom divider for UI

---
+++ Moonshot/CustomDivider.swift
@@ -0,0 +1,17 @@
+//
+//  CustomDivider.swift
+//  Moonshot
+//
+//  Created by Dechon Ryan on 5/15/24.
+//
+
+import SwiftUI
+
+struct CustomDivider: View {
+    var body: some View {
+        Rectangle()
+            .frame(height: 2)
+            .foregroundStyle(.lightBackground)
+            .padding(.vertical)
+    }
+}

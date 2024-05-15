e5a77f8cf516d979ff108c02f765bd680e73ea2d
Dechon <72828058+dech0n@users.noreply.github.com>
2024-05-14 18:16:59 -0700

    Create Astronaut type

---
+++ Moonshot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Dechon Ryan on 5/14/24.
+//
+
+import SwiftUI
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}

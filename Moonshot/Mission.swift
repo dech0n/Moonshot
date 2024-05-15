c8e8506b0972863b5f85811ee529242396f4be10
Dechon <72828058+dech0n@users.noreply.github.com>
2024-05-14 19:00:09 -0700

    Create Mission type

---
+++ Moonshot/Mission.swift
@@ -0,0 +1,21 @@
+//
+//  Mission.swift
+//  Moonshot
+//
+//  Created by Dechon Ryan on 5/14/24.
+//
+
+import Foundation
+
+
+struct Mission: Codable, Identifiable {
+    struct CrewRole: Codable {
+        let name: String
+        let role: String
+    }
+    
+    let id: Int
+    let launchDate: String?
+    let crew: [CrewRole]
+    let description: String
+}
